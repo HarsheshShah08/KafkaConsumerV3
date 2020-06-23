a2cf3a5510685aadb70c3b92b9e1f0e62af5b1fd
HarsheshShah08 <harshesh.shah@icloud.com>
2020-06-23 11:40:27 +0100

    first commit

---
+++ src/main/java/com/example/demo/KafkaConsumerV3.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class KafkaConsumerV3 {
+
+    public static void main(String[] args) {
+
+        //String consumerGroup = args[0];
+    	String consumerGroup = "dispatch-service";
+        System.out.printf("Consumer group: %s%n", consumerGroup);
+
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", consumerGroup);
+        props.setProperty("auto.offset.reset", "earliest");
+        props.setProperty("key.deserializer", IntegerDeserializer.class.getName());
+        props.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        KafkaConsumer<Integer, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(List.of("MyFirstTopicForDemo"));
+
+        while (true) {
+            ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(100));
+            for (var record : records) {
+                System.out.printf("Message '%s' at offset %d of partition %s%n", record.value(), record.offset(), record.partition());
+            }
+        }
+    }
+
+
+}
+
+
